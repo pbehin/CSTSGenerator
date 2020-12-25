584cf13c5525166c8164245f1435f6edf342691c
Peyman Behinaein <p.behinaein@gmail.com>
2020-12-25 05:30:51 +0330

    Fix Error In Git Build

--- src/Tests/TestInfrastructure/Dte.cs
+++ src/Tests/TestInfrastructure/Dte.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
 using EnvDTE;
 
 namespace Typewriter.Tests.TestInfrastructure
 {
     internal static class Dte
     {
-        private static DTE _dte = null;
+        private static DTE _dte;
         private static readonly object LockObj = new object();
 
         internal static DTE GetInstance(string solution)
@@ -16,31 +14,37 @@ namespace Typewriter.Tests.TestInfrastructure
             {
                 lock (LockObj)
                 {
-                    if (_dte == null)
+                    if (_dte != null) return _dte;
+                    Type dteType = null;
+                    for (int i = 20; i > 10; i--)
                     {
-
-                        Type dteType = null;
-                        for (int i = 20; i > 10; i--)
+                        try
                         {
-                            try
-                            {
-                                dteType = Type.GetTypeFromProgID($"VisualStudio.DTE.{i}.0", true);
-                                if (dteType != null)
-                                    break;
-                            }
-                            catch
-                            {
-                            }
-
+                            dteType = Type.GetTypeFromProgID($"VisualStudio.DTE.{i}.0", true);
+                            if (dteType != null)
+                                break;
                         }
+                        catch
+                        {
+                            // ignored
+                        }
+                    }
 
-                        if (dteType == null)
-                            throw new Exception("Can not find Dte.");
-                        _dte = (DTE) Activator.CreateInstance(dteType);
+                    if (dteType == null)
+                        throw new Exception("Cannot find Dte Type.");
+                    _dte = (DTE)Activator.CreateInstance(dteType);
+                    if (_dte == null)
+                        throw new TypeAccessException("Cannot Create Instance of DTE.");
+                    try
+                    {
+                        _dte.Solution.Open(solution);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception("Cannot Open Solution.", e);
                     }
                 }
             }
-            _dte.Solution.Open(solution);
             return _dte;
 
         }
